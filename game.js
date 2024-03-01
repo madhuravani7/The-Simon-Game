769dbe2600ea182ab6cbd0e782816a31f20dde51
madhuravani7 <madhubudige67@gmail.com>
2024-03-02 06:06:37 +1100

    animation added in game.js

--- game.js
+++ game.js
@@ -2,25 +2,36 @@ buttonColors = ["red", "blue", "green", "yellow"];
 gamePattern = [];
 userClickedPattern = [];
 
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+  });
+
 function nextSequence()
 {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
-    $(".btn").click(function(){
-       var userChosenColor = $(this).attr("id");
-       console.log("user clicked color " + userChosenColor)
-       userClickedPattern.push(userChosenColor);
-    });
-    
+    playSound(randomChosenColor);   
 };
 
-nextSequence();
 
 function playSound(name){
    var audio = new Audio ("sounds/" + name + ".mp3");
    audio.play();
 }
+
+function animatePress(currentColor){
+    $("#"+ currentColor).addClass("pressed");
+
+    setTimeout(function(){
+    $("#"+ currentColor).removeClass("pressed");
+    } , 100);
+}
+
+
 
